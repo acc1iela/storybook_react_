fb0a70e88f21960cf0faadcfd2fe6f95a70de04a
acc1iela <acc1iela@gmail.com>
2023-03-13 21:13:37 +0900

    update: 不要なコメント削除

--- src/components/Task.js
+++ src/components/Task.js
@@ -34,13 +34,11 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
   );
 }
 Task.propTypes = {
-  /** Composition of the task */
   task: PropTypes.shape({
-    /** Id of the task */
     id: PropTypes.string.isRequired,
-    /** Title of the task */
+
     title: PropTypes.string.isRequired,
-    /** Current state of the task */
+
     state: PropTypes.string.isRequired,
   }),
   /** Event to change the task to archived */
